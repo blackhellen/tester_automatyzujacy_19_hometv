594ca025ad35b9b994d77eac9bf0f98b82fe999c
blackhellen <m.k.fryt@gmail.com>
2025-08-01 21:01:16 +0200

    HelloWorld

--- esteban/src/HelloWord.java
+++
@@ -1,8 +0,0 @@
-class Main {
-    public static void main (String[] args) {
-        String imie="Mirek";
-        int wiek=35;
-        System.out.println("Cześć "+imie+" masz "+wiek+" lat");
-    }
-}
-// sdasdnlnsdalnsdalkndaskndas
