15222a31f3772ad1780cacdd0d295c1103044152
blackhellen <m.k.fryt@gmail.com>
2025-08-01 21:24:37 +0200

    nauka Zmienne

---
+++ nauka/Zmienne.java
@@ -0,0 +1,9 @@
+package nauka;
+
+public class Zmienne {
+    public static void main(String[] args) {
+        String imie="Esteban";
+        Integer wiek=35;
+        System.out.println("Cześć "+imie+" masz "+wiek+" lat");
+    }
+}
